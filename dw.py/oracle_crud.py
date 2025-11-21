619256a3065b85c5f8be1168cca83f67ed2328a3
Gonio-0 <dcgoniord14gp@gmail.com>
2025-11-21 12:42:38 -0300

    fghccn
    gyjgf

--- dw.py/oracle_crud.py
+++ dw.py/oracle_crud.py
@@ -200,7 +200,7 @@ def read_mascotas():
                     print(fila)
     except oracledb.DatabaseError as e:
         err = e
-        print(f"No se pudo crear la datos:")
+        print(f"No se pudo crear la datos: {err}")
     
 def read_mascotas_by_id(id):
     sql = (
@@ -218,7 +218,7 @@ def read_mascotas_by_id(id):
                     print(fila)
     except oracledb.DatabaseError as e:
         err = e
-        print(f"No se pudo crear la datos:")
+        print(f"No se pudo crear la datos: {err}")
 
 def read_perros():
     sql = (
@@ -233,7 +233,7 @@ def read_perros():
                     print(fila)
     except oracledb.DatabaseError as e:
         err = e
-        print(f"No se pudo crear la datos:")
+        print(f"No se pudo crear la datos: {err}")
 
 def read_perros_by_id(id):
     sql = (
@@ -251,7 +251,7 @@ def read_perros_by_id(id):
                     print(fila)
     except oracledb.DatabaseError as e:
         err = e
-        print(f"No se pudo crear la datos:")
+        print(f"No se pudo crear la datos: {err}")
 
 def read_gatos():
     sql = (
@@ -266,7 +266,7 @@ def read_gatos():
                     print(fila)
     except oracledb.DatabaseError as e:
         err = e
-        print(f"No se pudo crear la datos:")
+        print(f"No se pudo crear la datos: {err}")
 
 
 def read_gatos_by_id(id):
@@ -285,7 +285,7 @@ def read_gatos_by_id(id):
                     print(fila)
     except oracledb.DatabaseError as e:
         err = e
-        print(f"No se pudo crear la datos:")
+        print(f"No se pudo crear la datos: {err}")
 
 def read_aves():
     sql = (
@@ -300,7 +300,7 @@ def read_aves():
                     print(fila)
     except oracledb.DatabaseError as e:
         err = e
-        print(f"No se pudo crear la datos:")
+        print(f"No se pudo crear la datos: {err}")
 
 def read_aves_by_id(id):
     sql = (
@@ -318,4 +318,170 @@ def read_aves_by_id(id):
                     print(fila)
     except oracledb.DatabaseError as e:
         err = e
-        print(f"No se pudo crear la datos:")
+        print(f"No se pudo crear la datos: {err}")
+
+from typing import Optional
+def update_mascotas(
+    id,
+    nombre: Optional[str] = None,
+    edad: Optional[str] = None,
+    especie: Optional[str] = None,
+    
+):
+    modificaciones = []
+    parametros = {"id": id}
+
+    if nombre is not None:
+        modificaciones.append("nombre =: nombre")
+        parametros["nombre"] = nombre
+    if edad is not None:
+        modificaciones.append("edad =: edad")
+        parametros["edad"] = edad
+    if especie is not None:
+        modificaciones.append("espeice =: especie")
+        parametros["especie"] = especie
+    if not modificaciones:
+        return print("No hay campos para actualizar.")
+
+    sql = f"UPDATE mascotas SET {", ".join(modificaciones)} WHERE id =: id"
+
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(sql, parametros)
+        conn.commit()
+        print(f"Persona con nombre={nombre} actualizada.") 
+
+def delete_mascotas(id: int):
+    sql = (
+        "DELETE FROM MASCOTAS WHERE id = :id"
+    )
+
+    parametros = {"id" : id}
+
+    try:
+        with get_connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(sql, parametros)
+                conn.commit()
+                print(f"Dato eliminado \n {parametros} ")
+    except oracledb.DatabaseError as e:
+        err = e
+        print(f"Errormal eliminar dato: {err} \n {sql} \n {parametros}:")
+
+def delete_perros(id: int):
+    sql = (
+        "DELETE FROM PERROS WHERE id = :id"
+    )
+
+    parametros = {"id" : id}
+
+    try:
+        with get_connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(sql, parametros)
+                conn.commit()
+                print(f"Dato eliminado \n {parametros} ")
+    except oracledb.DatabaseError as e:
+        err = e
+        print(f"Errormal eliminar dato: {err} \n {sql} \n {parametros}:")
+
+def delete_gatos(id: int):
+    sql = (
+        "DELETE FROM GATOS WHERE id = :id"
+    )
+
+    parametros = {"id" : id}
+
+    try:
+        with get_connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(sql, parametros)
+                conn.commit()
+                print(f"Dato eliminado \n {parametros} ")
+    except oracledb.DatabaseError as e:
+        err = e
+        print(f"Errormal eliminar dato: {err} \n {sql} \n {parametros}:")
+
+def delete_AVES(id: int):
+    sql = (
+        "DELETE FROM AVES WHERE id = :id"
+    )
+
+    parametros = {"id" : id}
+
+    try:
+        with get_connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(sql, parametros)
+                conn.commit()
+                print(f"Dato eliminado \n {parametros} ")
+    except oracledb.DatabaseError as e:
+        err = e
+        print(f"Errormal eliminar dato: {err} \n {sql} \n {parametros}:")
+
+
+def menu_mascota():
+    while True:
+        os.system("cls")
+        print(
+            """
+            =======================================
+                         MENU MASCOTAS 
+            =======================================
+             1. INSERTAR MASCOTAS
+             2. LEER MASCOTAS
+             3. LEER MASCOTA POR ID
+             4. MODIFICAR MASCOTAS 
+             5. ELIMINAR MASCOTAS
+             0. SALIR
+            ======================================
+
+            """
+        )
+
+def main(cls):
+    while True:
+        os.system(cls)
+        print(
+            """
+            =======================================
+                       CRUD CON ORACLESQL
+            =======================================
+             1. APLICAR ESQUEMA EN LA BASE DE DATOS
+             2. TABLA MASCOTAS
+             3. TABLA PERROS
+             4. TABLA GATOS
+             5. TABLA AVES
+             0. SALIR
+            ======================================
+            
+            """
+        )
+        opcion = input("Selecciona una pcion [1=5, 0 para salir]")
+
+        if opcion == "0":
+            print("Adios")
+            input("Presiona ENTER para continuar...")
+            break
+        elif opcion == "1":
+            try:
+                id = input("Ingrese el id numerico de la mascota: ")
+                nombre = input("Ingrese el id numerico de la mascota: ")
+                especie = input("Ingrese el id numerico de la mascota: ")
+            except ValueError:
+                return print("ingresaste un valor no numerico")
+
+            create_mascotas(id,nombre,especie)
+            input("Presiona ENTER para continuar  ")
+
+        elif opcion == "2":
+            pass
+        elif opcion == "3":
+            pass
+        elif opcion == "4":
+            pass
+        elif opcion == "5":
+            pass
+
+if __name__ == "__name__":
+    main()
