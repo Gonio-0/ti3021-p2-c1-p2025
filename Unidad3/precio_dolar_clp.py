41a14c97567af5fe7c92c6b82c765c83db7b3e53
Gonio-0 <dcgoniord14gp@gmail.com>
2025-12-05 12:42:21 -0300

    yjhthht
    rgjhrggrgi

--- Unidad3/precio_dolar_clp.py
+++ Unidad3/precio_dolar_clp.py
@@ -2,8 +2,8 @@ import requests
 base_url = "https://cl.dolarapi.com"
 respuesta = requests.get(url=f"{base_url}/v1/cotizaciones/usd")
 codigo_respuesta = respuesta.status_code
-try:
-        data = respuesta.json
-        print(data)
+try: 
+    data = respuesta.json()
+    print(f"El valor del dolar contrastado con el peso chileno está en: {data["ultimoCierre"]} CLP")
 except:
-        print(respuesta)
+    print(respuesta)
