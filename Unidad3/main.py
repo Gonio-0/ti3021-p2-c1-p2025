9daef84c1000a0e5840bc3fe1da293b1c09d8124
Gonio-0 <dcgoniord14gp@gmail.com>
2025-12-12 12:54:11 -0300

    dsvvds
    vsddvs

--- Unidad3/main.py
+++ Unidad3/main.py
@@ -8,9 +8,13 @@ import bcrypt
 import requests
 #parametros opcionales
 from typing import Optional
-
+import datetime
 #Cargar las variables desde el archivo .env
 load_dotenv()
+
+username = os.getenv("ORACLE_USER")
+dsn = os.getenv("ORACLE_DSN")
+password = os.getenv("ORACLE_PASSWORD")
 
 class Database:
         def __init__(self,username,password,dsn):
@@ -21,14 +25,15 @@ class Database:
                 return oracledb.connect(user=self.username, password=self.password, dsn=self.dsn)
         def create_all_tables(self):
                 pass
-        def query(self, sql, parameters: optional[dict] = None):
-            print(f"ejecutando query:\n{sql}\n{parameters}")
+        def query(self, sql, parameters: Optional[dict]):
             try:
                 with self.get_connection() as conn:
                     with conn.cursor() as cur:
-                        resultados = cur.execute(statement=sql, parameters=parameters)
-                        for fila in resultados:
-                        print(fila)
+                        resultado = cur.execute(sql, parameters)
+                        return resultado
+                    conn.commit()
+            except oracledb.DatabaseError as error:
+                print(error)
 
 
 class Auth:
@@ -39,22 +44,46 @@ class Auth:
         def register(db: Database, username: str, password: str):
                 pass
 
+"""
+DOLAR
+EURO
+UD
+IVP
+IPC
+UTM
+"""
+
+
 class Finance:
-        def __init__(self, base_url: str):
+        def __init__(self, base_url: str = "https://mindicador.cl/api"):
             self.base_url = base_url
-        def get_uf (self):
-            pass
-        def get_uf (self):
-            pass
-        def get_uf (self):
-            pass
-        def get_uf (self):
-            pass
-        def get_uf (self):
-            pass
-        def get_uf (self):
-            pass
+        def get_indicator (self, indicator: str, fecha: None, ) -> int:
+            try:
+                if not fecha:    
+                    dd = datetime.datetime.now().day
+                    mm = datetime.datetime.now().month
+                    yyyy = datetime.datetime.now().year
+                    fecha = f"{dd}-{mm}-{yyyy}"  
+                url = f"{self.base_url}/{indicator}/{fecha}"
+                respuesta = requests.get(url).json()
+                print(respuesta["serie"][0]["valor"])
+            except:
+                  print("Hubo un error con la solicitud")
+        def get_usd (self, fecha: str = None):
+            valor = self.get_indicator("dolar",fecha)
+            print(f"El valor del dolar en CLP es: {valor}")
+        def get_eur (self, fecha: str = None):
+            self.get_indicator("eur",fecha)
+        def get_uf (self, fecha: str = None):
+            self.get_indicator("uf",fecha)
+        def get_uf (self, fecha: str = None):
+            self.get_indicator("ivp",fecha)
+        def get_uf (self, fecha: str = None):
+            self.get_indicator("utm",fecha)
     
+if __name__ == "__main__":
+    indicadores = Finance()
+    indicadores.get_usd("28-11-2025")
 
 if __name__ == "__main__":        
     db = Database(
@@ -62,5 +91,4 @@ if __name__ == "__main__":
        password=os.getenv("ORACLE_PASSWORD"),
        dsn=os.getenv("ORACKE_DSN")
     )
-
-db.query(sql="SELECT sysdate FROM dual")
+    print(db.query(sql="SELECT sysdate FROM dual"))
